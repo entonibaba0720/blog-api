07c935ed5ae4a5f513fd129d342ece48fe0e4698
hoffmannagnes <entonibaba0720@gmail.com>
2021-09-06 20:23:29 +0200

    fixed Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Blog API')
-    .setDescription('Simple blog API endpoints')
+    .setDescription('Documentation of a simple Blog API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('/', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
