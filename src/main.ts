10028034d75c4f92a7dd2c925250edfedfb88687
hoffmannagnes <entonibaba0720@gmail.com>
2021-09-06 20:46:40 +0200

    fixed deploy

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   };
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('/', app, document);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
