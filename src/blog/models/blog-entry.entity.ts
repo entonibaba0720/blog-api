54f5dec6e54324eceb73b004eacd9afd1a350519
hoffmannagnes <entonibaba0720@gmail.com>
2021-09-07 20:26:47 +0200

    change date format

--- src/blog/models/blog-entry.entity.ts
+++ src/blog/models/blog-entry.entity.ts
@@ -1,26 +1,26 @@
-import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
-import {UserEntity} from "../../user/models/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from '../../user/models/user.entity';
 
 @Entity('blog_entry')
 export class BlogEntryEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @ManyToOne((type) => UserEntity, (user) => user.blogEntries)
+  author: UserEntity;
 
-    @ManyToOne(type => UserEntity, user => user.blogEntries)
-    author: UserEntity;
+  @Column({
+    type: 'timestamp',
+    default: (order: 'DESC') => 'CURRENT_TIMESTAMP',
+  })
+  created: Date;
 
-    @Column({type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
-    created: Date;
+  @Column()
+  title: string;
 
-    @Column()
-    title: string;
+  @Column()
+  tags: string;
 
-    @Column()
-    tags: string;
-
-    @Column({default: ''})
-    body: string;
-
-
+  @Column({ default: '' })
+  body: string;
 }
